8d3747ef67d7f5099adad2f52450738a8efc9a89
ArnoldRamodike <riccoarnold23@gmail.com>
2024-04-10 23:33:46 +0200

    Global imports

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import UpcomingEvents from "./home/upcoming-events";
+import DealsCaharts from "./home/deals-charts";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+
+export {UpcomingEvents, DealsCaharts, UpcomingEventsSkeleton, KanbanColumnSkeleton,
+     LatestActivitiesSkeleton, ProjectCardSkeleton, AccordionHeaderSkeleton};
